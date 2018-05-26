6caf7529b8c27ee0ea384760c18afc0506e126f3
Yosemir <Yosemir@vallegrande.edu.pe>
2018-05-26 10:38:14 -0500

    Ultimos cambios

--- Lab01/QueryOne.sql
+++ Lab01/QueryOne.sql
@@ -15,3 +15,36 @@ USE AdventureWorks2016
 GO
 SELECT name from sys.tables;
 GO 
+
+/*ver listado de vistas de una BD*/
+USE AdventureWorks2016;
+GO
+SELECT name from sys.views;
+GO
+
+/*vizualizar detallada de una BD*/
+EXEC sp_helpdb adventureWorks2016;
+GO
+
+/*Utilizando snipets*/
+-- Create a new database called 'ColegioBD'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'ColegioBD'
+)
+CREATE DATABASE ColegioBD
+GO
+
+/*Eliminar una BD*/
+DROP DATABASE Hospital;
+GO
+/*eliminar la BD adventureWorks*/
+USE AdventureWorks2016;
+go
+DROP DATABASE AdventureWorks2016;
+GO
